9df1808a39c2fd258fc32dfb52a07493946c1df1
Luz Vedoya <lvedoya@udesa.edu.ar>
2021-04-07 13:24:42 -0300

    Hice la lista de productos y agregue la fotos

--- ProyectoIntegrador/productos/ropa.js
+++ ProyectoIntegrador/productos/ropa.js
@@ -1,16 +1,182 @@
 let ropa = {
-    listaRopa: [{
-
-//falta la lista de productos porque sino le mandamos todos los productos al index 
-    },
-    {
-
-    },
-
-
-
-
-]
-}; 
+    lista: [
+        {
+            id: 1,
+            tipo: "jean",
+            descripcion: "SLIM FIT - RELAXED LEG - HIGH RISE",
+            eco: "Algodón cultivado utilizando fertilizantes y pesticidas naturales. Además, en su cultivo no se utilizan semillas modificadas genéticamente, lo que ayuda a conservar la biodiversidad de las semillas y la fertilidad del suelo.",
+            comentarios: [{usuario: "user 1", comentario : "Muy buen calce y calidad. Lo recomiendo"},
+            {usuario: "user 2", comentario : "Este modelo me encanta! Super canchero y a la moda"},
+            {usuario: "user 3", comentario : "No me quedo muy bien por la forma de mis piernas :("},
+            {usuario: "user 4", comentario : "Excelente calidad y muy bueno el servicio."},
+            {usuario: "user 5", comentario : "Este catalogo es buenisimo, me encanta"}]
+        },
+        {
+            id: 2,
+            tipo: "jean",
+            descripcion: "THE SKINNY IN FLY BUTTONS IN CLOUD GREY.",
+            eco: "Algodón cultivado utilizando fertilizantes y pesticidas naturales. Además, en su cultivo no se utilizan semillas modificadas genéticamente, lo que ayuda a conservar la biodiversidad de las semillas y la fertilidad del suelo.",
+            comentarios: [{usuario: "user 1", comentario : "Me encanto el color y la calidad"},
+            {usuario: "user 2", comentario : "Malisimo el servicio. Se me traba todo el tiempo la computadora"},
+            {usuario: "user 3", comentario : "No me quedo el talle que compre! Pero el pantalon y el servicio geniales"},
+            {usuario: "user 4", comentario : "Un basico para que todos tengamos! Muy lindo todo"},
+            {usuario: "user 5", comentario : "No me quedo muy bien, pero consegui cambiarlo sin ningun prblema!!!"}]
+        },
+        {
+            id: 3,
+            tipo: "jean",
+            descripcion: "SLIM FIT - RELAXED LEG - HIGH RISE",
+            eco: "Algodón cultivado utilizando fertilizantes y pesticidas naturales. Además, en su cultivo no se utilizan semillas modificadas genéticamente, lo que ayuda a conservar la biodiversidad de las semillas y la fertilidad del suelo.",
+            comentarios: [{usuario: "user 1", comentario : "Muy lindo el calce y la manera en la cae la pierna ancha!! Me encanto!"},
+            {usuario: "user 2", comentario : "Siemper todo impecable en este catalogo, lo recomiendo"},
+            {usuario: "user 3", comentario : "Me llego roto el jean y con las costuras mal hechas! Necesitan control de calidad :/"},
+            {usuario: "user 4", comentario : "Muy lindo el modelo y un amor todos los de asistencia"},
+            {usuario: "user 5", comentario : "Me quiero comprar todo lo que veo aca! Todo divino!!"}]
+        },
+        {
+            id: 4,
+            tipo: "jean",
+            descripcion: "HIGH-WAISTED JEANS WITH FIVE POCKETS. WASHED EFFECT AND RIPPED DETAIL. FRONT ZIP AND METAL BUTTON CLOSURE.",
+            eco: "Algodón cultivado utilizando fertilizantes y pesticidas naturales. Además, en su cultivo no se utilizan semillas modificadas genéticamente, lo que ayuda a conservar la biodiversidad de las semillas y la fertilidad del suelo.",
+            comentarios: [{usuario: "user 1", comentario : "Tengo uno igual de otro color y son lo mas!"},
+            {usuario: "user 2", comentario : "Muy canchera la rotura en la rodilla! Me encanto todo"},
+            {usuario: "user 3", comentario : "Estoy navegando por la pagina online. Me quiero comprar todo!!"},
+            {usuario: "user 4", comentario : "Me comunique con asistencia al consumidor y todo impecable. Gracias!"},
+            {usuario: "user 5", comentario : "El talle que compre online de uno similar no me quedo bien, pero consegui cambiarlo sin ningun prblema!!!"}]
+        },
+        {
+            id: 5,
+            tipo: "novedad",
+            descripcion: "VESTIDO DE ESCOTE PICO Y MANGA LARGA. DETALLE TEJIDO DRAPEADO. BAJO CON ABERTURA LATERAL.",
+            eco: "Algodón cultivado utilizando fertilizantes y pesticidas naturales. Además, en su cultivo no se utilizan semillas modificadas genéticamente, lo que ayuda a conservar la biodiversidad de las semillas y la fertilidad del suelo.",
+            comentarios: [{usuario: "user 1", comentario : "Divino! La tela muy linda y muy elegante"},
+            {usuario: "user 2", comentario : "No me quedo bien el talle, pero lo pude cambiar sin ningun problema"},
+            {usuario: "user 3", comentario : "Imposible que te quede bien este vestido!! No lo recomiendo"},
+            {usuario: "user 4", comentario : "Me encanta como esta dispuesto todo en el catalogo"},
+            {usuario: "user 5", comentario : "Me enamore de este vestido cuando lo vi!!"}]
+        },
+        {
+            id: 6,
+            tipo: "novedad",
+            descripcion: "VESTIDO CORTO DE ESCOTE PICO PRONUNCIADO. MANGA LARGA ABULLONADA CON HOMBRERAS ACABADA EN PUÑO. CIERRE EN ESPALDA CON CREMALLERA OCULTA EN COSTURA.",
+            eco: "Algodón cultivado utilizando fertilizantes y pesticidas naturales. Además, en su cultivo no se utilizan semillas modificadas genéticamente, lo que ayuda a conservar la biodiversidad de las semillas y la fertilidad del suelo.",
+            comentarios: [{usuario: "user 1", comentario : "Muy bueno este vestido. Un basico para combinar con diferentes accesorios."},
+            {usuario: "user 2", comentario : "Muy linda la tela y muy buena la calidad de todo!"},
+            {usuario: "user 3", comentario : "Me encantan todos los vestidosssss"},
+            {usuario: "user 4", comentario : "Muy buena la asistencia al cliente"},
+            {usuario: "user 5", comentario : "Me encanta ver como hay cada vez mas gente que aprovecha este catalogo, es lo mas!!!"}]
+        },
+        {
+            id: 7,
+            tipo: "novedad",
+            descripcion: "VESTIDO CORTO DE ESCOTE PICO PRONUNCIADO. MANGA LARGA ABULLONADA CON HOMBRERAS ACABADA EN PUÑO. CIERRE EN ESPALDA CON CREMALLERA OCULTA EN COSTURA. CON CADENAS PLATEADAS DECORADAS CON PIEDRAS BRILLANTES.",
+            eco: "Algodón cultivado utilizando fertilizantes y pesticidas naturales. Además, en su cultivo no se utilizan semillas modificadas genéticamente, lo que ayuda a conservar la biodiversidad de las semillas y la fertilidad del suelo.",
+            comentarios: [{usuario: "user 1", comentario : "Me habia comprado hace unas semanas uno similar pero sin las cadenitas. Este es mucho mas lindo! Me encanta!"},
+            {usuario: "user 2", comentario : "Se me traba la computadora cuando entro a este catalogo"},
+            {usuario: "user 3", comentario : "Muy lindo todo!!"},
+            {usuario: "user 4", comentario : "Contesten este mensaje para unirse a un grupo de este catalogo"},
+            {usuario: "user 5", comentario : "Este modelo me encanta! Super canchero y a la moda"}]
+        },
+        {
+            id: 8,
+            tipo: "novedad",
+            descripcion: "CUERPO DE CUELLO REDONDO Y ESCOTE PICO. MANGA LARGA. CINTURÓN TIPO CORDÓN COMBINADO A TONO. BAJO CON ABERTURAS LATERALES.",
+            eco: "Algodón cultivado utilizando fertilizantes y pesticidas naturales. Además, en su cultivo no se utilizan semillas modificadas genéticamente, lo que ayuda a conservar la biodiversidad de las semillas y la fertilidad del suelo.",
+            comentarios: [{usuario: "user 1", comentario : "Me parece muy dificil que un talle de esto quede bien tanto la parte de arriba como la de abajo"},
+            {usuario: "user 2", comentario : "Muy lindo y muy elegante"},
+            {usuario: "user 3", comentario : "Me encantaaaa!"},
+            {usuario: "user 4", comentario : "Me encanta este nuevo modelo!! Pueden mostrarlo en otros colores?"},
+            {usuario: "user 5", comentario : "Me compre uno parecido hace unas semanas y me lo ponderan siempre!"}]
+        },
+        {
+            id: 9,
+            tipo: "remera",
+            descripcion: "CAMISETA DE CUELLO REDONDO Y MANGA CORTA. DETALLE DE ESTAMPACIÓN COMBINADA A CONTRASTE DE MICKEY MOUSE ©DISNEY.",
+            eco: "Algodón cultivado utilizando fertilizantes y pesticidas naturales. Además, en su cultivo no se utilizan semillas modificadas genéticamente, lo que ayuda a conservar la biodiversidad de las semillas y la fertilidad del suelo.",
+            comentarios: [{usuario: "user 1", comentario : "Me encanta la nueva linea con productos ed Disney!"},
+            {usuario: "user 2", comentario : "Se la compre para mi hija adolescente. Me dijo que ya no usaba cosas de Mickey porque son para mas chicos, pero ahora no se la saca mas jajajaja"},
+            {usuario: "user 3", comentario : "Que se ponga de moda Mickey!!!"},
+            {usuario: "user 4", comentario : "Van a mostrar una linea de remeras con otros personas de Disney?? Me encantaria!!"},
+            {usuario: "user 5", comentario : "Comenten los fans de Disney!"}]
+        },
+        {
+            id: 10,
+            tipo: "remera",
+            descripcion: "CAMISETA DE CUELLO REDONDO Y MANGA SISA. DETALLE DE HOMBRERAS.",
+            eco: "Algodón cultivado utilizando fertilizantes y pesticidas naturales. Además, en su cultivo no se utilizan semillas modificadas genéticamente, lo que ayuda a conservar la biodiversidad de las semillas y la fertilidad del suelo.",
+            comentarios: [{usuario: "user 1", comentario : "Muy linda la tela y muy buena la calidad"},
+            {usuario: "user 2", comentario : "Me encanta! Me la compraria en todos los colores"},
+            {usuario: "user 3", comentario : "Por fin volvieron las hombreras!!"},
+            {usuario: "user 4", comentario : "Para tener un usuario en el catalogo hay que pagar?"},
+            {usuario: "user 5", comentario : "Me compre una igual en Zara hace un par de semanas y no me llego! Si no se contactan conmigo los voy a escrachar!"}]
+        },
+        {
+            id: 11,
+            tipo: "remera",
+            descripcion: "CAMISETA CORTA DE CUELLO REDONDO Y MANGA CORTA. DETALLE DE APLICACIÓN TERCIOPELO COMBINADA A CONTRASTE EN DELANTERO.",
+            eco: "Algodón cultivado utilizando fertilizantes y pesticidas naturales. Además, en su cultivo no se utilizan semillas modificadas genéticamente, lo que ayuda a conservar la biodiversidad de las semillas y la fertilidad del suelo.",
+            comentarios: [{usuario: "user 1", comentario : "Muy lindaa!"},
+            {usuario: "user 2", comentario : "No me quedo bien el talle, pero pude cambiarla sin problemas"},
+            {usuario: "user 3", comentario : "Recomiendo mucho la compra online! Confien en los talles"},
+            {usuario: "user 4", comentario : "Me encanto esta remera, muy linda la frase que dice en el frente"},
+            {usuario: "user 5", comentario : "Me llego hoy y la frase esta mal escrita, espero poder efectuar un cambio"}]
+        },
+        {
+            id: 12,
+            tipo: "remera",
+            descripcion: "CAMISETA CORTA DE CUELLO REDONDO Y MANGA CORTA. DETALLE DE BOLSILLO DE JEAN EN DELANTERO.",
+            eco: "Algodón cultivado utilizando fertilizantes y pesticidas naturales. Además, en su cultivo no se utilizan semillas modificadas genéticamente, lo que ayuda a conservar la biodiversidad de las semillas y la fertilidad del suelo.",
+            comentarios: [{usuario: "user 1", comentario : "Muy lindo el detalle del bolsillo de jean"},
+            {usuario: "user 2", comentario : "Estuve hoy en el local, un amor todo el personal"},
+            {usuario: "user 3", comentario : "No me quedo bien el talle, pero pude cambiarla sin problemas"},
+            {usuario: "user 4", comentario : "Me encantan todas las remeras de esta marca. Tiene algunas muy simples como esta y otrs mas elegantes"},
+            {usuario: "user 5", comentario : "Muy lindo todo en la pagina web"}]
+        },
+        {
+            id: 13,
+            tipo: "zapato",
+            descripcion: "ZAPATO TIPO BOTA ALTA PLANA DE COLOR NEGRO. TIRADOR EN LA PARTE TRASERA INFERIOR. SUELA GRUESA. CIERRE MEDIANTE CORDONES Y CREMALLERA INTERIOR.",
+            eco: "Trabajamos con nuestros proveedores, trabajadores, sindicatos y organismos internacionales para desarrollar una cadena de suministro en la que se respetan y promueven los derechos humanos, contribuyendo a los Objetivos de Desarrollo Sostenible de Naciones Unidas.",
+            comentarios: [{usuario: "user 1", comentario : "Muy cancheras las botas!!"},
+            {usuario: "user 2", comentario : "Tengo unas botas de este local hace años y estan impecables! Muy buena la calidad"},
+            {usuario: "user 3", comentario : "Estoy hace varios dias intentando comunicarme con asistencia al cliente y nadie me contesta"},
+            {usuario: "user 4", comentario : "Estaba buscando este modelo hace semanas para mi hija, por fin las encuentro aca!!"},
+            {usuario: "user 5", comentario : "No me quedo bien el talle, pero pude cambiarla sin problemas"}]
+        },
+        {
+            id: 14,
+            tipo: "zapato",
+            descripcion: "ZAPATO TIPO BOTÍN PLANO DE PIEL DE COLOR NEGRO. CORTE DE PIEL CON ELÁSTICOS EN LOS LATERALES. TIRADOR TRASERO. SUELA GRUESA TIPO TRACK.",
+            eco: "Trabajamos con nuestros proveedores, trabajadores, sindicatos y organismos internacionales para desarrollar una cadena de suministro en la que se respetan y promueven los derechos humanos, contribuyendo a los Objetivos de Desarrollo Sostenible de Naciones Unidas.",
+            comentarios: [{usuario: "user 1", comentario : "Se me rompio la suela en el primer uso! Malisimo!"},
+            {usuario: "user 2", comentario : "Muy bueno el servicio en el local! Respetan mucho el protocolo por pandemia pero los zapatos se pueden probar"},
+            {usuario: "user 3", comentario : "Estos mirando la pagina web por primera vez, muy buena!"},
+            {usuario: "user 4", comentario : "Me encantaaaaaaan!"},
+            {usuario: "user 5", comentario : "Muy lindos todos los zapatos que se ven la pagina web"}]
+        },
+        {
+            id: 15,
+            tipo: "zapato",
+            descripcion: "ZAPATO TIPO BOTÍN DE TACÓN DE COLOR BEIGE. CORTE ELÁSTICO. TACÓN FINO FORRADO. ACABADO EN PUNTA. CIERRE MEDIANTE CREMALLERA EN EL INTERIOR.",
+            eco: "Trabajamos con nuestros proveedores, trabajadores, sindicatos y organismos internacionales para desarrollar una cadena de suministro en la que se respetan y promueven los derechos humanos, contribuyendo a los Objetivos de Desarrollo Sostenible de Naciones Unidas.",
+            comentarios: [{usuario: "user 1", comentario : "Muy delicado y elegante, me encanta"},
+            {usuario: "user 2", comentario : "Los use para una fiesta y siguen impecables. Muy buena calidad"},
+            {usuario: "user 3", comentario : "Me encanta todo lo que veo en la pagina!"},
+            {usuario: "user 4", comentario : "Muy buena la asistencia al cliente tanto online como en el local"},
+            {usuario: "user 5", comentario : "Muy bueno el servicio en el local! Respetan mucho el protocolo por pandemia pero los zapatos se pueden probar"}]
+        },
+        {
+            id: 16,
+            tipo: "zapato",
+            descripcion: "ZAPATO TIPO BOTÍN DE TACÓN DE COLOR NEGRO. DETALLE DE MAXI CADENA EN LA PARTE DELANTERA. TACÓN FINO FORRADO. ACABADO EN PUNTA. CIERRE MEDIANTE CREMALLERA EN EL INTERIOR. AIRFIT®. PLANTILLA TÉCNICA FLEXIBLE DE ESPUMA COMPUESTA DE LÁTEX, DISEÑADA PARA OFRECER UN MAYOR CONFORT. ALTURA DEL TACÓN: 9,5 CM.",
+            eco: "Trabajamos con nuestros proveedores, trabajadores, sindicatos y organismos internacionales para desarrollar una cadena de suministro en la que se respetan y promueven los derechos humanos, contribuyendo a los Objetivos de Desarrollo Sostenible de Naciones Unidas.",
+            comentarios: [{usuario: "user 1", comentario : "Muy original el detalle de la cadera en la parte de adelante."},
+            {usuario: "user 2", comentario : "Se me despego la cadena en el primer uso"},
+            {usuario: "user 3", comentario : "Me encantan! Los tengo desde otra temporada y los gasto"},
+            {usuario: "user 4", comentario : "Muy delicado el zapato, me encanta"},
+            {usuario: "user 5", comentario : "Muy lindo todo en el local!"}]
+        },
+    ]
+}
 
 module.exports = ropa;
