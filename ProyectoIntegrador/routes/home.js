9fd9519de3e8488cf0d1c2ded1d0e640173bb756
Luz Vedoya <lvedoya@udesa.edu.ar>
2021-04-16 16:16:35 -0300

    cambio de tipografias y css

--- ProyectoIntegrador/routes/home.js
+++ ProyectoIntegrador/routes/home.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+          
 let controlador = require('../controladores/controller');
 
 router.get('/', controlador.home);
@@ -8,7 +8,7 @@ router.get('/', controlador.home);
 router.get('/product/:id', controlador.porId);
    
 router.get('/login', controlador.login);  
-
+  
 router.get('/register', controlador.register);
                    
 router.get('/search', controlador.search); 
