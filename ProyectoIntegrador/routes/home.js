3b255d6cfe0821cee12f7a2f4b9ff8e16beb859c
Martina <mtomlianovich@udesa.edu.ar>
2021-04-17 10:18:35 -0300

    logo y header

--- ProyectoIntegrador/routes/home.js
+++ ProyectoIntegrador/routes/home.js
@@ -6,13 +6,13 @@ let controlador = require('../controladores/controller');
 router.get('/', controlador.home);
 
 router.get('/product/:id', controlador.porId);
-   
+    
 router.get('/login', controlador.login);  
   
 router.get('/register', controlador.register);
                    
 router.get('/search', controlador.search); 
-
+ 
 router.get('/profile', controlador.profile);
 
 router.get('/productadd', controlador.productadd);
